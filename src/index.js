80e452e11e3a7e08e8bbadbe06ca5664b64c9440
Fahim Khan <fk652@nyu.edu>
2023-08-18 12:01:49 -0400

    setup redux store

--- src/index.js
+++ src/index.js
@@ -1,11 +1,31 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import configureStore from './store/store';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+let store = configureStore({});
+
+function Root() {
+  return (
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  );
+}
+
+const renderApplication = () => {
+  const root = ReactDOM.createRoot(document.getElementById('root'));
+
+  root.render(
+    <React.StrictMode>
+      <Root />
+    </React.StrictMode>
+  );
+}
+
+renderApplication();
